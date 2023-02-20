9141b57b7fae2b0a7a4fda9d42dd0533f5654c28
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-02-19 22:58:33 -0800

    Fix site loading issues

--- static/js/load-site.js
+++ static/js/load-site.js
@@ -1,25 +1,22 @@
-$(document).ready(function() {
-	// Will wait for everything on the page to load.
-	$(window).bind('load', function() {
-		$('.overlay, body').addClass('loaded');
-		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-    }, 2000)
+$(window).bind('load', function() {
+  $('.overlay, body').addClass('loaded');
+  setTimeout(function() {
+    $('.overlay').css({'display':'none'})
+  }, 2000)
 
-	});
-	var options = {
-    strings: ['<span>> Miguel </span><span style="color: #D8C787;">VF</span>'],
-		typeSpeed: 75, 
-		backSpeed: 50,
-		smartBackspace: true, 
-		showCursor: true, 
-		cursorChar: '_', 
-    attr: null
-	};
-	
-	// Will remove overlay after 30 sec for users cannnot load properly.
-	setTimeout(function() {
-    $('.overlay, body').addClass('loaded');
-    var typed = new Typed('.typewriter', options);
-	}, 3000);
-})
+});
+var options = {
+  strings: ['<span>> Miguel </span><span style="color: #D8C787;">VF</span>'],
+  typeSpeed: 75, 
+  backSpeed: 50,
+  smartBackspace: true, 
+  showCursor: true, 
+  cursorChar: '_', 
+  attr: null
+};
+
+// Will remove overlay after 30 sec for users cannnot load properly.
+setTimeout(function() {
+  $('.overlay, body').addClass('loaded');
+  var typed = new Typed('.typewriter', options);
+}, 3000);
