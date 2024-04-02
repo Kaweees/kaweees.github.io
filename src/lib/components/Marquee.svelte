2b5f3c7e8f9c1120b53c2619a238df0776c3418d
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-04-01 22:24:25 -0700

    Make marquee pause on hover

--- src/lib/components/Marquee.svelte
+++ src/lib/components/Marquee.svelte
@@ -4,13 +4,11 @@
 	export let pauseOnHover = false;
 	export let reverse = false;
 	export let fade = false;
-	export let className;
+	export let className = '';
 </script>
 
 <div
-	class={'group flex overflow-hidden' +
-		(direction === 'left' ? 'flex-row ' : 'flex-col ') +
-		className}
+	class="group flex overflow-hidden {direction === 'left' ? 'flex-row' : 'flex-col'} {className}"
 	style="mask-image: {fade
 		? 'linear-gradient(' +
 			(direction === 'left' ? 'to right' : 'to bottom') +
@@ -19,16 +17,16 @@
 		? 'linear-gradient(' +
 			(direction === 'left' ? 'to right' : 'to bottom') +
 			', transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)'
-		: undefined};
-    gap: {gap};"
+		: undefined}; gap: {gap};"
 >
 	{#each [0, 1] as i}
 		<div
 			style="--gap: {gap};"
-			class={'flex shrink-0 justify-around gap-[var(--gap)] ' +
-				(direction === 'left' ? 'animate-marquee-left flex-row ' : 'animate-marquee-up flex-col ') +
-				(pauseOnHover ? 'group-hover:animation-play-state:paused ' : '') +
-				(reverse ? 'direction-reverse' : '')}
+			class="flex shrink-0 justify-around gap-[var(--gap)] {direction === 'left'
+				? 'animate-marquee-left flex-row'
+				: 'animate-marquee-up flex-col'} {pauseOnHover
+				? 'group-hover:[animation-play-state:paused]'
+				: ''} {reverse ? 'direction-reverse' : ''}"
 		>
 			<slot />
 		</div>
