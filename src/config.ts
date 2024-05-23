9462c252f6d8681f81f6ba20594987623d50cffa
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-05-23 00:35:48 -0700

    Add theme colors

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,13 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
+export const NAV_LINKS = [
+  { href: "about", title: "About" },
+  { href: "projects", title: "Projects" },
+  { href: "posts", title: "Posts" },
+  { href: "resume", title: "Resume" },
+];
+
 export const LOCALE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
   langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
